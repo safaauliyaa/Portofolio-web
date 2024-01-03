5f9094cea74bc7a9a61709b21f58eff80d833e43
Safa Auliya <safaauliya19@gmail.com>
2024-01-03 18:04:55 +0700

    hero section & navbar revision link

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Hero
+  
+    // Button
+    document.getElementById('contactButton').addEventListener('click', function() {
+      window.location.href = 'mailto:safaauliya19@gmail.com';
+    });
+  
+    // End Hero
+  });
