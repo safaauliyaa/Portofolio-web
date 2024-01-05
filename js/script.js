fda7c8fe5cd8a522910bb5e29b655f6fe7e4fbf1
Safa Auliya <safaauliya19@gmail.com>
2024-01-05 23:12:03 +0700

    about section

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     // Hero
-  
+    // console.log('DOM LOADED')
     // Button
     document.getElementById('contactButton').addEventListener('click', function() {
       window.location.href = 'mailto:safaauliya19@gmail.com';
